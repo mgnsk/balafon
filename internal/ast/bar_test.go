e6b4600e2115ed3eed9a1452b201bb413fc5f31a
Magnus Kokk <magnus@kokk.eu>
2022-10-16 19:37:40 +0300

    Harden ast tests

--- internal/ast/bar_test.go
+++ internal/ast/bar_test.go
@@ -2,22 +2,28 @@ package ast_test
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
+	"github.com/mgnsk/gong/internal/ast"
 	. "github.com/onsi/gomega"
 )
 
 func TestBar(t *testing.T) {
 	g := NewGomegaWithT(t)
 
-	input1 := `assign c 60
+	input1 := `
+
+assign c 60
 assign d 62
 bar "Bar 1" {
 	start
 	c4
 	stop
 }
-play "Bar 1"`
+play "Bar 1"
+
+`
 
 	input2 := `assign c 60; assign d 62
 bar "Bar 1" { start; c; stop }
@@ -26,11 +32,14 @@ play "Bar 1"
 
 	res1, err := parse(input1)
 	g.Expect(err).NotTo(HaveOccurred())
-	g.Expect(fmt.Sprint(res1)).To(Equal(input1))
+	g.Expect(res1).To(BeAssignableToTypeOf(ast.DeclList{}))
+
+	g.Expect(fmt.Sprint(res1)).To(Equal(strings.Trim(input1, " \n")))
 
 	res2, err := parse(input2)
 	g.Expect(err).NotTo(HaveOccurred())
-	g.Expect(fmt.Sprint(res2)).To(Equal(input1))
-
+	g.Expect(res2).To(BeAssignableToTypeOf(ast.DeclList{}))
 	g.Expect(res2).To(Equal(res1))
+
+	g.Expect(fmt.Sprint(res2)).To(Equal(strings.Trim(input1, " \n")))
 }
