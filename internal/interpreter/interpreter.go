df567d38d7e67af06ca02f8f35e1205dbac15f03
Magnus Kokk <magnus@kokk.eu>
2022-10-20 20:06:35 +0300

    Rewrite interpreter for new ast.DeclList

--- internal/interpreter/interpreter.go
+++ internal/interpreter/interpreter.go
@@ -1,12 +1,8 @@
 package interpreter
 
 import (
-	"bufio"
-	"errors"
 	"fmt"
-	"io"
 	"sort"
-	"strings"
 
 	"github.com/mgnsk/gong/internal/ast"
 	"github.com/mgnsk/gong/internal/constants"
@@ -32,15 +28,12 @@ type midiKey struct {
 
 // Interpreter evaluates messages from raw line input.
 type Interpreter struct {
-	parser       *parser.Parser
-	keymap       map[midiKey]uint8
-	ringing      map[midiKey]struct{}
-	bars         map[string]sequencer.Events
-	barName      string
-	barBuffer    sequencer.Events
-	curBarLength smf.MetricTicks
-	curChannel   uint8
-	curVelocity  uint8
+	parser      *parser.Parser
+	keymap      map[midiKey]uint8
+	ringing     map[midiKey]struct{}
+	bars        map[string]sequencer.Events
+	curChannel  uint8
+	curVelocity uint8
 }
 
 var sugInsideBar = []string{
@@ -53,6 +46,8 @@ var sugInsideBar = []string{
 	"end",
 }
 
+// TODO: generate those in init()
+// by parsing a parse error?
 var sugOutsideBar = []string{
 	"assign",
 	"tempo",
@@ -63,6 +58,105 @@ var sugOutsideBar = []string{
 	"bar",
 	"start",
 	"stop",
+}
+
+// TODO: parseOption: fillBarSilence - fills bars with
+
+func (it *Interpreter) Eval(input string) (sequencer.Events, error) {
+	res, err := it.parser.Parse(lexer.NewLexer([]byte(input)))
+	if err != nil {
+		return nil, err
+	}
+
+	declList, ok := res.(ast.DeclList)
+	if !ok {
+		return nil, fmt.Errorf("invalid input")
+	}
+
+	return it.ParseAST(declList)
+}
+
+func (it *Interpreter) ParseAST(declList ast.DeclList) (sequencer.Events, error) {
+	var events sequencer.Events
+
+	for _, decl := range declList {
+		switch decl := decl.(type) {
+		case ast.Bar:
+			if _, ok := it.bars[decl.Name]; ok {
+				return nil, fmt.Errorf("bar '%s' already defined", decl.Name)
+			}
+
+			barEvents, err := it.ParseAST(decl.DeclList)
+			if err != nil {
+				return nil, err
+			}
+
+			it.bars[decl.Name] = barEvents
+
+		case ast.CmdAssign:
+			if err := it.assign(it.curChannel, decl.Note, decl.Key); err != nil {
+				return nil, err
+			}
+
+		case ast.CmdTempo:
+			events = append(events, &sequencer.Event{
+				Message: smf.MetaTempo(float64(decl)),
+			})
+
+		case ast.CmdTimeSig:
+			// it.curBarLength = smf.MetricTicks(decl.Num) * (constants.TicksPerWhole / smf.MetricTicks(decl.Denom))
+			events = append(events, &sequencer.Event{
+				Message: smf.MetaMeter(decl.Num, decl.Denom),
+			})
+
+		case ast.CmdVelocity:
+			it.curVelocity = uint8(decl)
+
+		case ast.CmdChannel:
+			it.curChannel = uint8(decl)
+
+		case ast.CmdProgram:
+			events = append(events, &sequencer.Event{
+				Message: smf.Message(midi.ProgramChange(it.curChannel, uint8(decl))),
+			})
+
+		case ast.NoteList:
+			noteEvents, err := it.parseNoteList(decl)
+			if err != nil {
+				return nil, err
+			}
+
+			events = append(events, noteEvents...)
+
+		case ast.CmdControl:
+			events = append(events, &sequencer.Event{
+				Message: smf.Message(midi.ControlChange(it.curChannel, decl.Control, decl.Parameter)),
+			})
+
+		case ast.CmdPlay:
+			// TODO
+			// bar, ok := it.bars[string(decl)]
+			// if !ok {
+			// 	return bar, nil
+			// }
+			// return nil, fmt.Errorf("unknown bar in command %s", decl.String())
+
+		case ast.CmdStart:
+			events = append(events, &sequencer.Event{
+				Message: smf.Message(midi.Start()),
+			})
+
+		case ast.CmdStop:
+			events = append(events, &sequencer.Event{
+				Message: smf.Message(midi.Stop()),
+			})
+
+		default:
+			panic(fmt.Sprintf("invalid token %T", decl))
+		}
+	}
+
+	return events, nil
 }
 
 // Suggest returns suggestions for the next input.
@@ -96,119 +190,66 @@ func (it *Interpreter) Suggest() []string {
 // }
 
 // Parse a single input line into an AST node.
-func (it *Interpreter) Parse(input string) (interface{}, error) {
-	if len(strings.TrimSpace(input)) == 0 {
-		return nil, nil
-	}
+// func (it *Interpreter) Parse(input string) (interface{}, error) {
+// 	if len(strings.TrimSpace(input)) == 0 {
+// 		return nil, nil
+// 	}
 
-	it.parser.Reset()
+// 	it.parser.Reset()
 
-	return it.parser.Parse(lexer.NewLexer([]byte(input)))
-}
+// 	return it.parser.Parse(lexer.NewLexer([]byte(input)))
+// }
 
 // NoteOn creates a real time note on event on zero tick with an optional preceding NoteOff if the note was ringing.
 // All notes are left ringing.
-func (it *Interpreter) NoteOn(note rune) ([]midi.Message, error) {
-	key, ok := it.getKey(note)
-	if !ok {
-		return nil, fmt.Errorf("note '%c' undefined", note)
-	}
+// func (it *Interpreter) NoteOn(note rune) ([]midi.Message, error) {
+// 	key, ok := it.getKey(note)
+// 	if !ok {
+// 		return nil, fmt.Errorf("note '%c' undefined", note)
+// 	}
 
-	velocity := it.curVelocity
-	ms := make([]midi.Message, 0, 2)
+// 	velocity := it.curVelocity
+// 	ms := make([]midi.Message, 0, 2)
 
-	if it.isRinging(note) {
-		it.setRingingOff(note)
-		ms = append(ms, midi.NoteOff(it.curChannel, key))
-	}
+// 	if it.isRinging(note) {
+// 		it.setRingingOff(note)
+// 		ms = append(ms, midi.NoteOff(it.curChannel, key))
+// 	}
 
-	ms = append(ms, midi.NoteOn(it.curChannel, key, velocity))
+// 	ms = append(ms, midi.NoteOn(it.curChannel, key, velocity))
 
-	it.setRingingOn(note)
+// 	it.setRingingOn(note)
 
-	return ms, nil
-}
+// 	return ms, nil
+// }
 
 // Eval evaluates a single input line.
 // If both return values are nil, more input is needed.
-func (it *Interpreter) Eval(input string) (sequencer.Events, error) {
-	res, err := it.Parse(input)
-	if err != nil {
-		return nil, err
-	}
+// func (it *Interpreter) Eval(input string) (sequencer.Events, error) {
+// 	res, err := it.Parse(input)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	if res == nil {
-		// Skip comments.
-		return nil, nil
-	}
+// 	if res == nil {
+// 		// Skip comments.
+// 		return nil, nil
+// 	}
 
-	switch r := res.(type) {
-	case ast.NoteList:
-		return it.addNotes(r)
+// 	switch r := res.(type) {
+// 	case ast.NoteList:
+// 		return it.addNotes(r)
 
-	case ast.CmdAssign:
-		if err := it.assign(r.Note, r.Key); err != nil {
-			return nil, err
-		}
-		return nil, nil
+// 	case ast.CmdAssign:
+// 		if err := it.assign(r.Note, r.Key); err != nil {
+// 			return nil, err
+// 		}
+// 		return nil, nil
 
-	case ast.CmdTempo:
-		return it.appendCommand(smf.MetaTempo(float64(r)))
-
-	case ast.CmdTimeSig:
-		events, err := it.appendCommand(smf.MetaMeter(r.Num, r.Denom))
-		if err != nil {
-			return nil, err
-		}
-		it.curBarLength = smf.MetricTicks(r.Num) * (constants.TicksPerWhole / smf.MetricTicks(r.Denom))
-		return events, nil
-
-	case ast.CmdChannel:
-		it.curChannel = uint8(r)
-		return nil, nil
-
-	case ast.CmdVelocity:
-		it.curVelocity = uint8(r)
-		return nil, nil
-
-	case ast.CmdProgram:
-		return it.appendCommand(midi.ProgramChange(it.curChannel, uint8(r)))
-
-	case ast.CmdControl:
-		return it.appendCommand(midi.ControlChange(it.curChannel, r.Control, r.Parameter))
-
-	case ast.CmdBar:
-		if err := it.startBar(string(r)); err != nil {
-			return nil, err
-		}
-		return nil, nil
-
-	case ast.CmdEnd:
-		if err := it.endBar(); err != nil {
-			return nil, err
-		}
-		return nil, nil
-
-	case ast.CmdPlay:
-		if it.barName != "" {
-			return nil, it.errBarNotEnded("play bar")
-		}
-		events, ok := it.bars[string(r)]
-		if !ok {
-			return nil, fmt.Errorf("bar '%s' does not exist", string(r))
-		}
-		return events, nil
-
-	case ast.CmdStart:
-		return it.appendCommand(midi.Start())
-
-	case ast.CmdStop:
-		return it.appendCommand(midi.Stop())
-
-	default:
-		panic(fmt.Sprintf("invalid expression: %#v", r))
-	}
-}
+// 	default:
+// 		panic(fmt.Sprintf("invalid expression: %#v", r))
+// 	}
+// }
 
 func getTimeSig(event *sequencer.Event) [2]uint8 {
 	var num, denom, c, ds uint8
@@ -219,60 +260,60 @@ func getTimeSig(event *sequencer.Event) [2]uint8 {
 }
 
 // EvalAll evaluates all messages from r.
-func (it *Interpreter) EvalAll(r io.Reader) (*sequencer.Song, error) {
-	s := bufio.NewScanner(r)
+// func (it *Interpreter) EvalAll(r io.Reader) (*sequencer.Song, error) {
+// 	s := bufio.NewScanner(r)
 
-	var errs strings.Builder
+// 	var errs strings.Builder
 
-	var bar sequencer.Bar
-	song := sequencer.New()
+// 	var bar sequencer.Bar
+// 	song := sequencer.New()
 
-	line := 0
-	for s.Scan() {
-		line++
+// 	line := 0
+// 	for s.Scan() {
+// 		line++
 
-		if events, err := it.Eval(s.Text()); err != nil {
-			errs.WriteString(fmt.Sprintf("[%d]: %s\n", line, err))
-		} else if events != nil {
-			hasNotes := false
+// 		if events, err := it.Eval(s.Text()); err != nil {
+// 			errs.WriteString(fmt.Sprintf("[%d]: %s\n", line, err))
+// 		} else if events != nil {
+// 			hasNotes := false
 
-			for _, event := range events {
-				switch event.Message.Type() {
-				case smf.MetaTimeSigMsg:
-					bar.TimeSig = getTimeSig(event)
-				case midi.NoteOnMsg:
-					hasNotes = true
-				}
-				bar.Events = append(bar.Events, event)
-			}
+// 			for _, event := range events {
+// 				switch event.Message.Type() {
+// 				case smf.MetaTimeSigMsg:
+// 					bar.TimeSig = getTimeSig(event)
+// 				case midi.NoteOnMsg:
+// 					hasNotes = true
+// 				}
+// 				bar.Events = append(bar.Events, event)
+// 			}
 
-			if hasNotes {
-				song.AddBar(bar)
-				bar = sequencer.Bar{}
-			}
-		}
-	}
+// 			if hasNotes {
+// 				song.AddBar(bar)
+// 				bar = sequencer.Bar{}
+// 			}
+// 		}
+// 	}
 
-	if err := s.Err(); err != nil {
-		return nil, err
-	}
+// 	if err := s.Err(); err != nil {
+// 		return nil, err
+// 	}
 
-	if errs.Len() > 0 {
-		return nil, errors.New(errs.String())
-	}
+// 	if errs.Len() > 0 {
+// 		return nil, errors.New(errs.String())
+// 	}
 
-	if len(bar.Events) > 0 {
-		song.AddBar(bar)
-	}
+// 	if len(bar.Events) > 0 {
+// 		song.AddBar(bar)
+// 	}
 
-	song.SetBarAbsTicks()
+// 	song.SetBarAbsTicks()
 
-	return song, nil
-}
+// 	return song, nil
+// }
 
-func (it *Interpreter) errBarNotEnded(want string) error {
-	return fmt.Errorf("cannot %s: bar '%s' is not ended", want, it.barName)
-}
+// func (it *Interpreter) errBarNotEnded(want string) error {
+// 	return fmt.Errorf("cannot %s: bar '%s' is not ended", want, it.curBar)
+// }
 
 // func (it *Interpreter) play(events sequencer.Events) {
 // 	// Offset relative messages to absolute tick.
@@ -305,7 +346,7 @@ func (it *Interpreter) parseNoteList(noteList ast.NoteList) (sequencer.Events, e
 			continue
 		}
 
-		key, ok := it.getKey(note.Name)
+		key, ok := it.getKey(it.curChannel, note.Name)
 		if !ok {
 			return nil, fmt.Errorf("note '%c' undefined", note.Name)
 		}
@@ -333,7 +374,7 @@ func (it *Interpreter) parseNoteList(noteList ast.NoteList) (sequencer.Events, e
 		}
 
 		if it.isRinging(note.Name) {
-			// TODO
+			// TODO: midi dur 0?
 			it.setRingingOff(note.Name)
 		}
 
@@ -368,16 +409,16 @@ func (it *Interpreter) parseNoteList(noteList ast.NoteList) (sequencer.Events, e
 	return events, nil
 }
 
-func (it *Interpreter) getKey(note rune) (uint8, bool) {
-	key, ok := it.keymap[midiKey{it.curChannel, note}]
+func (it *Interpreter) getKey(channel uint8, note rune) (uint8, bool) {
+	key, ok := it.keymap[midiKey{channel, note}]
 	return key, ok
 }
 
-func (it *Interpreter) assign(note rune, key uint8) error {
-	if k, ok := it.getKey(note); ok {
-		return fmt.Errorf("note '%c' already assigned to '%d' on channel '%d'", note, k, it.curChannel)
+func (it *Interpreter) assign(channel uint8, note rune, key uint8) error {
+	if key, ok := it.getKey(channel, note); ok {
+		return fmt.Errorf("note '%c' already assigned to key '%d' on channel '%d'", note, key, it.curChannel)
 	}
-	it.keymap[midiKey{it.curChannel, note}] = key
+	it.keymap[midiKey{channel, note}] = key
 	return nil
 }
 
@@ -394,73 +435,32 @@ func (it *Interpreter) setRingingOff(note rune) {
 	delete(it.ringing, midiKey{it.curChannel, note})
 }
 
-func (it *Interpreter) startBar(name string) error {
-	if it.barName != "" {
-		return it.errBarNotEnded("begin bar")
-	}
-	if _, ok := it.bars[name]; ok {
-		return fmt.Errorf("bar '%s' already defined", name)
-	}
-	it.barName = name
-	return nil
-}
+// func (it *Interpreter) addNotes(list ast.NoteList) (sequencer.Events, error) {
+// 	if it.curBar != "" {
+// 		if it.curBarLength > 0 {
+// 			var barLength smf.MetricTicks
+// 			for _, note := range list {
+// 				barLength += note.Ticks()
+// 			}
+// 			if barLength != it.curBarLength {
+// 				return nil, fmt.Errorf("invalid bar length")
+// 			}
+// 		}
+// 		events, err := it.parseNoteList(list)
+// 		if err != nil {
+// 			return nil, err
+// 		}
+// 		it.barBuffer = append(it.barBuffer, events...)
+// 		return nil, nil
+// 	}
 
-func (it *Interpreter) endBar() error {
-	if it.barName == "" {
-		return errors.New("cannot end bar: no active bar")
-	}
-	if len(it.barBuffer) == 0 {
-		return fmt.Errorf("invalid bar '%s'", it.barName)
-	}
-	// TODO
-	// sort.Sort(it.barBuffer)
-	it.bars[it.barName] = it.barBuffer
-	it.barName = ""
-	it.barBuffer = nil
-	it.curBarLength = 0
-	return nil
-}
+// 	events, err := it.parseNoteList(list)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-func (it *Interpreter) addNotes(list ast.NoteList) (sequencer.Events, error) {
-	if it.barName != "" {
-		if it.curBarLength > 0 {
-			var barLength smf.MetricTicks
-			for _, note := range list {
-				barLength += note.Ticks()
-			}
-			if barLength != it.curBarLength {
-				return nil, fmt.Errorf("invalid bar length")
-			}
-		}
-		events, err := it.parseNoteList(list)
-		if err != nil {
-			return nil, err
-		}
-		it.barBuffer = append(it.barBuffer, events...)
-		return nil, nil
-	}
-
-	events, err := it.parseNoteList(list)
-	if err != nil {
-		return nil, err
-	}
-
-	return events, nil
-}
-
-func (it *Interpreter) appendCommand(msg []byte) (sequencer.Events, error) {
-	event := &sequencer.Event{
-		Message: smf.Message(msg),
-	}
-	if it.barName != "" {
-		if find(it.barBuffer, midi.NoteOnMsg) >= 0 {
-			return nil, fmt.Errorf("timesig command must be at the beginning of bar")
-		}
-		it.barBuffer = append(it.barBuffer, event)
-		return nil, nil
-	}
-	return sequencer.Events{event}, nil
-}
+// 	return events, nil
+// }
 
 // case ast.CmdControl:
 // 	event := &sequencer.Event{
--- internal/interpreter/interpreter_test.go
+++ internal/interpreter/interpreter_test.go
@@ -2,13 +2,9 @@ package interpreter_test
 
 import (
 	"bytes"
-	"fmt"
 	"io/ioutil"
-	"strings"
 	"testing"
-	"time"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/mgnsk/gong/internal/constants"
 	"github.com/mgnsk/gong/internal/interpreter"
 	. "github.com/onsi/gomega"
@@ -219,133 +215,7 @@ func TestNoteLengths(t *testing.T) {
 	}
 }
 
-func TestCommandForbiddenInBar(t *testing.T) {
-	for _, input := range []string{
-		`bar "forbidden"`,
-		`play "forbidden"`,
-	} {
-		t.Run(input, func(t *testing.T) {
-			g := NewWithT(t)
-
-			it := interpreter.New()
-
-			evalExpectNil(g, it, `bar "bar"`)
-
-			ms, err := it.Eval(input)
-			g.Expect(err).To(HaveOccurred())
-			g.Expect(err.Error()).To(ContainSubstring("not ended"))
-			g.Expect(ms).To(BeNil())
-		})
-	}
-}
-
-func TestInvalidBarLength(t *testing.T) {
-	g := NewWithT(t)
-
-	it := interpreter.New()
-
-	evalExpectNil(g, it, `bar "bar"`)
-	evalExpectNil(g, it, `timesig 3 8`)
-
-	ms, err := it.Eval("[kk]8")
-	g.Expect(err).To(HaveOccurred())
-	g.Expect(err.Error()).To(ContainSubstring("invalid bar length"))
-	g.Expect(ms).To(BeNil())
-}
-
-func TestTempoAndTimeSigInBar(t *testing.T) {
-	g := NewWithT(t)
-
-	it := interpreter.New()
-
-	evalExpectNil(g, it, "assign k 36")
-	evalExpectNil(g, it, "assign s 38")
-	evalExpectNil(g, it, `bar "bar"`)
-	evalExpectNil(g, it, `tempo 200`)
-	evalExpectNil(g, it, `timesig 1 4`)
-	evalExpectNil(g, it, `k`)
-	evalExpectNil(g, it, `s`)
-	evalExpectNil(g, it, `end`)
-
-	ms, err := it.Eval(`play "bar"`)
-	g.Expect(err).NotTo(HaveOccurred())
-	g.Expect(ms).To(HaveLen(4))
-
-	// TODO: consistof in any order
-
-	g.Expect(ms[0].Pos).To(Equal(uint8(0)))
-	g.Expect(ms[0].Message).To(Equal(smf.Message(midi.Message(smf.MetaTempo(200)))))
-
-	g.Expect(ms[1].Pos).To(Equal(uint8(0)))
-	g.Expect(ms[1].Message).To(Equal(smf.Message(midi.Message(smf.MetaMeter(1, 4)))))
-
-	g.Expect(ms[2].Pos).To(Equal(uint8(0)))
-	g.Expect(ms[2].Message).To(Equal(smf.Message(midi.NoteOn(0, 36, 127))))
-
-	g.Expect(ms[3].Pos).To(Equal(uint8(0)))
-	g.Expect(ms[3].Message).To(Equal(smf.Message(midi.NoteOn(0, 38, 127))))
-
-	// g.Expect(ms[4].Pos).To(Equal(uint32(constants.TicksPerQuarter)))
-	// g.Expect(ms[4].Message).To(Equal(midi.NoteOff(0, 36)))
-
-	// g.Expect(ms[5].Pos).To(Equal(uint32(constants.TicksPerQuarter)))
-	// g.Expect(ms[5].Message).To(Equal(midi.NoteOff(0, 38)))
-}
-
-func TestBar(t *testing.T) {
-	g := NewWithT(t)
-
-	it := interpreter.New()
-
-	for _, input := range []string{
-		`velocity 127`,
-		`channel 1`,
-		`assign x 36`,
-		`channel 2`,
-		`assign x 38`,
-		``,
-		`bar "verse"`,
-		`velocity 100`,
-		`channel 1`,
-		`x`,
-		`channel 2`,
-		`x`,
-		`end`,
-		// The channel is still 2 and velocity 100.
-		`bar "default"`,
-		`x`,
-		`end`,
-	} {
-		evalExpectNil(g, it, input)
-	}
-
-	ms, err := it.Eval(`play "verse"`)
-	g.Expect(err).NotTo(HaveOccurred())
-	g.Expect(ms).To(HaveLen(2))
-
-	g.Expect(ms[0].Pos).To(Equal(uint8(0)))
-	g.Expect(ms[0].Message).To(Equal(smf.Message(midi.NoteOn(1, 36, 100))))
-
-	g.Expect(ms[1].Pos).To(Equal(uint8(0)))
-	g.Expect(ms[1].Message).To(Equal(smf.Message(midi.NoteOn(2, 38, 100))))
-
-	// TODO
-	// g.Expect(ms[2].Pos).To(Equal(uint32(constants.TicksPerQuarter)))
-	// g.Expect(ms[2].Message).To(Equal(midi.NoteOff(1, 36)))
-
-	// g.Expect(ms[3].Pos).To(Equal(uint32(constants.TicksPerQuarter)))
-	// g.Expect(ms[3].Message).To(Equal(midi.NoteOff(2, 38)))
-
-	ms, err = it.Eval(`play "default"`)
-	g.Expect(err).NotTo(HaveOccurred())
-	g.Expect(ms).To(HaveLen(1))
-
-	g.Expect(ms[0].Pos).To(Equal(uint8(0)))
-	g.Expect(ms[0].Message).To(Equal(smf.Message(midi.NoteOn(2, 38, 100))))
-
-	// g.Expect(ms[1].Pos).To(Equal(uint32(constants.TicksPerQuarter * 2)))
-	// g.Expect(ms[1].Message).To(Equal(midi.NoteOff(2, 38)))
-}
+// TODO: test bar length validation
 
 func TestLetRing(t *testing.T) {
 	g := NewWithT(t)
@@ -375,34 +245,34 @@ func TestLetRing(t *testing.T) {
 	// g.Expect(ms[2].Message).To(Equal(smf.Message(midi.NoteOff(0, 36))))
 }
 
-func TestEvalAll(t *testing.T) {
-	g := NewWithT(t)
+// func TestEvalAll(t *testing.T) {
+// 	g := NewWithT(t)
 
-	it := interpreter.New()
+// 	it := interpreter.New()
 
-	input := `
-		velocity 100
-		channel 1
-		assign x 36
-		channel 2
-		assign x 38
+// 	input := `
+// 		velocity 100
+// 		channel 1
+// 		assign x 36
+// 		channel 2
+// 		assign x 38
 
-		bar "verse"
-		channel 1
-		x
-		channel 2
-		x
-		end
-        play "verse"
-    `
-	// TODO bar not filled error
+// 		bar "verse"
+// 		channel 1
+// 		x
+// 		channel 2
+// 		x
+// 		end
+//         play "verse"
+//     `
+// 	// TODO bar not filled error
 
-	song, err := it.EvalAll(strings.NewReader(input))
-	g.Expect(err).NotTo(HaveOccurred())
+// 	song, err := it.EvalAll(strings.NewReader(input))
+// 	g.Expect(err).NotTo(HaveOccurred())
 
-	spew.Dump(song)
+// 	spew.Dump(song)
 
-}
+// }
 
 var (
 	testFile  []byte
@@ -418,28 +288,28 @@ func init() {
 	lineCount = bytes.Count(testFile, []byte{'\n'})
 }
 
-func BenchmarkInterpreter(b *testing.B) {
-	start := time.Now()
+// func BenchmarkInterpreter(b *testing.B) {
+// 	start := time.Now()
 
-	b.ReportAllocs()
-	b.ResetTimer()
+// 	b.ReportAllocs()
+// 	b.ResetTimer()
 
-	var err error
+// 	var err error
 
-	for i := 0; i < b.N; i++ {
-		it := interpreter.New()
-		_, err = it.EvalAll(bytes.NewReader(testFile))
-	}
+// 	for i := 0; i < b.N; i++ {
+// 		it := interpreter.New()
+// 		_, err = it.EvalAll(bytes.NewReader(testFile))
+// 	}
 
-	b.StopTimer()
+// 	b.StopTimer()
 
-	if err != nil {
-		panic(err)
-	}
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	elapsed := time.Since(start)
+// 	elapsed := time.Since(start)
 
-	linesPerNano := float64(b.N*lineCount) / float64(elapsed)
+// 	linesPerNano := float64(b.N*lineCount) / float64(elapsed)
 
-	fmt.Printf("lines per second: %f\n", linesPerNano*float64(time.Second))
-}
+// 	fmt.Printf("lines per second: %f\n", linesPerNano*float64(time.Second))
+// }
