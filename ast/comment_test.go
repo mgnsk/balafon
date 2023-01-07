ffaf48a96de838c9b6dad4ae2acfa6e1d4a9b8b9
Magnus Kokk <magnus@kokk.eu>
2023-01-07 17:22:35 +0200

    Prepare testcase for linecomment parse errors

--- ast/comment_test.go
+++ ast/comment_test.go
@@ -10,19 +10,37 @@ import (
 func TestLineComment(t *testing.T) {
 	g := NewGomegaWithT(t)
 
-	res, err := parse("// this is a line comment\n")
+	res, err := parse(`
+	// this is a line comment
+
+	assign c 60
+`)
 	g.Expect(err).NotTo(HaveOccurred())
-	g.Expect(res).To(Equal(ast.NodeList{ast.LineComment(" this is a line comment")}))
+	g.Expect(res).To(Equal(ast.NodeList{
+		ast.LineComment(" this is a line comment"),
+		ast.CmdAssign{Note: 'c', Key: 60},
+	}))
 }
 
 func TestBlockComment(t *testing.T) {
 	g := NewGomegaWithT(t)
 
-	res, err := parse("/*this is a block comment*/")
+	res, err := parse(`
+/* this is a block comment */
+assign c 60
+`)
 	g.Expect(err).NotTo(HaveOccurred())
-	g.Expect(res).To(Equal(ast.NodeList{ast.BlockComment("this is a block comment")}))
+	g.Expect(res).To(Equal(ast.NodeList{
+		ast.BlockComment(" this is a block comment "),
+		ast.CmdAssign{Note: 'c', Key: 60},
+	}))
 
-	res, err = parse("/*\nthis is a\nblock comment\n*/")
+	res, err = parse(`
+/*
+this is a
+block comment
+*/
+`)
 	g.Expect(err).NotTo(HaveOccurred())
 	g.Expect(res).To(Equal(ast.NodeList{ast.BlockComment("\nthis is a\nblock comment\n")}))
 }
