ec0044ed5ac6cc5f37efbbc4fb8929a1174bed56
Magnus Kokk <magnus@kokk.eu>
2023-07-25 16:57:07 +0300

    Add test for live shell

--- live_test.go
+++ live_test.go
@@ -1,17 +1,25 @@
 package balafon_test
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/mgnsk/balafon"
+	"github.com/mgnsk/balafon/internal/constants"
+	. "github.com/onsi/gomega"
+	"gitlab.com/gomidi/midi/v2"
 	"gitlab.com/gomidi/midi/v2/drivers"
 )
 
 type out struct {
 	drivers.Port
+	buf *bytes.Buffer
 }
 
-func (*out) Send([]byte) error {
+func (o *out) Send(b []byte) error {
+	if o.buf != nil {
+		o.buf.Write(b)
+	}
 	return nil
 }
 
@@ -20,6 +28,21 @@ type reader struct{}
 func (*reader) Read(p []byte) (int, error) {
 	p[0] = 'a'
 	return 1, nil
+}
+
+func TestLiveShell(t *testing.T) {
+	g := NewWithT(t)
+
+	it := balafon.New()
+	g.Expect(it.EvalString(":assign a 60")).To(Succeed())
+
+	buf := &bytes.Buffer{}
+
+	s := balafon.NewLiveShell(&reader{}, it, &out{buf: buf})
+	g.Expect(s.HandleNext()).To(Succeed())
+
+	msg := midi.Message(buf.Bytes())
+	g.Expect(msg).To(Equal(midi.NoteOn(0, 60, constants.DefaultVelocity)))
 }
 
 func BenchmarkLiveShell(b *testing.B) {
