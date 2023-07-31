22a9f9f6b401b8b118b967fa0c280c98559b1194
Magnus Kokk <magnus@kokk.eu>
2023-07-31 22:11:09 +0300

    Implement initial MusicXML render

--- xml.go
+++ xml.go
@@ -1,14 +1,31 @@
 package balafon
 
 import (
-	"time"
+	"bytes"
+	"encoding/xml"
+	"strconv"
 
-	"github.com/eliothedeman/mxl"
+	"github.com/mgnsk/balafon/internal/constants"
 	"golang.org/x/exp/slices"
 )
 
+var notes = []string{"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"}
+
+// GetPitch returns the step and octave for MIDI note.
+func GetPitch(note uint8) (step string, octave uint8) {
+	octave = (note / 12) - 1
+	noteIndex := note % 12
+
+	return notes[noteIndex], octave
+}
+
+type xmlTrack struct {
+	channel int
+	bars    []Bar
+}
+
 // ToXML converts a balafon script to MusicXML.
-func ToXML(input []byte) (*mxl.MXLDoc, error) {
+func ToXML(input []byte) ([]byte, error) {
 	it := New()
 
 	if err := it.Eval(input); err != nil {
@@ -17,83 +34,240 @@ func ToXML(input []byte) (*mxl.MXLDoc, error) {
 
 	bars := it.Flush()
 
-	parts := map[uint8]*mxl.Part{}
+	channels := map[uint8]*xmlTrack{}
+	for _, bar := range bars {
+		for _, ev := range bar.Events {
+			t, ok := channels[ev.Track]
+			if !ok {
+				t = &xmlTrack{}
+				channels[ev.Track] = t
+			}
+		}
+	}
 
-	for barNo, bar := range bars {
-		tracks := map[uint8][]Event{}
+	parts := map[uint8]*Part{}
+
+	for i, bar := range bars {
+		events := map[uint8][]Event{}
 
 		for _, ev := range bar.Events {
-			tracks[ev.Track] = append(tracks[ev.Track], ev)
+			events[ev.Track] = append(events[ev.Track], ev)
 		}
 
-		for trackNo, trackEvents := range tracks {
-			part, ok := parts[trackNo]
+		for ch := range channels {
+			p, ok := parts[ch]
 			if !ok {
-				part = &mxl.Part{}
-				parts[trackNo] = part
+				p = &Part{
+					Id: strconv.Itoa(int(ch)),
+				}
+				parts[ch] = p
 			}
 
-			measure := mxl.Measure{
-				Number: barNo,
-				Atters: mxl.Attributes{
-					Time: mxl.Time{
+			measure := Measure{
+				Number: i,
+				Atters: Attributes{
+					Time: &Time{
 						Beats:    int(bar.TimeSig[0]),
 						BeatType: int(bar.TimeSig[1]),
 					},
-					Divisions: int(bar.Cap()), // TODO
+					Divisions: int(constants.TicksPerWhole),
+					// Key       Key  `xml:"key"`
+					// Time      Time `xml:"time"`
+					// Divisions int  `xml:"divisions"`
+					// Clef      Clef `xml:"clef"`
+
 				},
 			}
 
-			for _, ev := range trackEvents {
-				if ev.Note != nil {
-					measure.Notes = append(measure.Notes, mxl.Note{
-						Duration: int(ev.Note.Props.NoteLen()),
-						// Type: "whole", // TODO
+			if barEvents, ok := events[ch]; ok {
+				for _, ev := range barEvents {
+					if ev.Note != nil {
+						if ev.Note.IsPause() {
+							measure.Notes = append(measure.Notes, Note{
+								// Pitch: mxl.Pitch{
+								// 	// Accidental int8   `xml:"alter"`
+								// 	Step:   "C",
+								// 	Octave: 4,
+								// },
+								Duration: int(ev.Note.Props.NoteLen()),
+								// Voice    int      `xml:"voice"`
+								// Type     string   `xml:"type"`
+								Rest: &xml.Name{
+									Local: "rest",
+								},
+								// Chord    xml.Name `xml:"chord"`
+								// Tie      Tie      `xml:"tie"`
 
-						// Pitch    Pitch    `xml:"pitch"`
-						// Duration int      `xml:"duration"`
-						// Voice    int      `xml:"voice"`
-						// Type     string   `xml:"type"`
-						// Rest     xml.Name `xml:"rest"`
-						// Chord    xml.Name `xml:"chord"`
-						// Tie      Tie      `xml:"tie"`
-					})
+							})
+						} else {
+							var c, k, v uint8
+							if !ev.Message.GetNoteStart(&c, &k, &v) {
+								panic("expected GetNoteStart() to succeed")
+							}
+
+							step, octave := GetPitch(k)
+
+							measure.Notes = append(measure.Notes, Note{
+								Pitch: Pitch{
+									// Accidental int8   `xml:"alter"`
+									Step:   step,
+									Octave: int(octave),
+								},
+								Duration: int(ev.Note.Props.NoteLen()),
+								// Voice    int      `xml:"voice"`
+								// Type     string   `xml:"type"`
+								// Chord    xml.Name `xml:"chord"`
+								// Tie      Tie      `xml:"tie"`
+							})
+						}
+					}
 				}
 			}
 
-			part.Measures = append(part.Measures, measure)
+			p.Measures = append(p.Measures, measure)
 		}
 	}
 
 	type trackPart struct {
-		track int
-		part  mxl.Part
+		ch   uint8
+		part *Part
+	}
+	tps := make([]trackPart, 0, len(parts))
+	for ch, p := range parts {
+		tps = append(tps, trackPart{
+			ch:   ch,
+			part: p,
+		})
+	}
+	slices.SortFunc(tps, func(a, b trackPart) bool {
+		return a.ch < b.ch
+	})
+
+	finalParts := make([]Part, len(tps))
+	for i := 0; i < len(tps); i++ {
+		finalParts[i] = *tps[i].part
 	}
 
-	trackParts := make([]trackPart, 0, len(parts))
-	for trackNo, p := range parts {
-		trackParts = append(trackParts, trackPart{
-			track: int(trackNo),
-			part:  *p,
+	score := Score{
+		Version: "4.0",
+		Parts:   finalParts,
+	}
+
+	for _, p := range parts {
+		score.PartList.Parts = append(score.PartList.Parts, ScorePart{
+			ID:   p.Id,
+			Name: p.Id,
 		})
 	}
 
-	slices.SortFunc(trackParts, func(a, b trackPart) bool {
-		return a.track < b.track
-	})
+	var buf bytes.Buffer
 
-	finalParts := make([]mxl.Part, 0, len(parts))
-	for _, p := range trackParts {
-		finalParts = append(finalParts, p.part)
+	buf.WriteString(`<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+	<!DOCTYPE score-partwise PUBLIC
+	"-//Recordare//DTD MusicXML 4.0 Partwise//EN"
+	"http://www.musicxml.org/dtds/partwise.dtd">
+	`)
+
+	enc := xml.NewEncoder(&buf)
+	enc.Indent("", "    ")
+	if err := enc.Encode(score); err != nil {
+		return nil, err
 	}
 
-	return &mxl.MXLDoc{
-		Identification: mxl.Identification{
-			Encoding: mxl.Encoding{
-				Software: "balafon",
-				Date:     time.Now().Format(time.RFC3339),
-			},
-		},
-		Parts: finalParts,
-	}, nil
+	return buf.Bytes(), nil
+}
+
+// Score holds all data for a music xml file
+type Score struct {
+	XMLName        xml.Name        `xml:"score-partwise"`
+	Version        string          `xml:"version,attr"`
+	Identification *Identification `xml:"identification,omitempty"`
+	PartList       PartList        `xml:"part-list,omitempty"`
+	Parts          []Part          `xml:"part,omitempty"`
+}
+
+type PartList struct {
+	Parts []ScorePart `xml:"score-part,omitempty"`
+}
+
+type ScorePart struct {
+	ID   string `xml:"id,attr"`
+	Name string `xml:"part-name"`
+}
+
+// Identification holds all of the ident information for a music xml file
+type Identification struct {
+	Composer string    `xml:"creator,omitempty"`
+	Encoding *Encoding `xml:"encoding,omitempty"`
+	Rights   string    `xml:"rights,omitempty"`
+	Source   string    `xml:"source,omitempty"`
+	Title    string    `xml:"movement-title,omitempty"`
+}
+
+// Encoding holds encoding info
+type Encoding struct {
+	Software string `xml:"software,omitempty"`
+	Date     string `xml:"encoding-date,omitempty"`
+}
+
+// Part represents a part in a piece of music
+type Part struct {
+	Id       string    `xml:"id,attr"`
+	Measures []Measure `xml:"measure"`
+}
+
+// Measure represents a measure in a piece of music
+type Measure struct {
+	Number int        `xml:"number,attr"`
+	Atters Attributes `xml:"attributes"`
+	Notes  []Note     `xml:"note"`
+}
+
+// Attributes represents
+type Attributes struct {
+	Key       *Key  `xml:"key,omitempty"`
+	Time      *Time `xml:"time,omitempty"`
+	Divisions int   `xml:"divisions,omitempty"`
+	Clef      *Clef `xml:"clef,omitempty"`
+}
+
+// Clef represents a clef change
+type Clef struct {
+	Sign string `xml:"sign"`
+	Line int    `xml:"line"`
+}
+
+// Key represents a key signature change
+type Key struct {
+	Fifths int    `xml:"fifths"`
+	Mode   string `xml:"mode"`
+}
+
+// Time represents a time signature change
+type Time struct {
+	Beats    int `xml:"beats"`
+	BeatType int `xml:"beat-type"`
+}
+
+// Note represents a note in a measure
+type Note struct {
+	Pitch    Pitch     `xml:"pitch"`
+	Duration int       `xml:"duration"`
+	Voice    int       `xml:"voice,omitempty"`
+	Type     string    `xml:"type,omitempty"`
+	Rest     *xml.Name `xml:"rest,omitempty"`
+	Chord    *xml.Name `xml:"chord,omitempty"`
+	Tie      *Tie      `xml:"tie,omitempty"`
+}
+
+// Pitch represents the pitch of a note
+type Pitch struct {
+	Accidental int8   `xml:"alter,omitempty"`
+	Step       string `xml:"step"`
+	Octave     int    `xml:"octave"`
+}
+
+// Tie represents whether or not a note is tied.
+type Tie struct {
+	Type string `xml:"type,attr"`
 }
--- xml_test.go
+++ xml_test.go
@@ -1,7 +1,6 @@
 package balafon_test
 
 import (
-	"encoding/xml"
 	"fmt"
 	"testing"
 
@@ -9,13 +8,49 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+func TestGetPitch(t *testing.T) {
+	g := NewWithT(t)
+
+	step, octave := balafon.GetPitch(60)
+	g.Expect(step).To(Equal("C"))
+	g.Expect(octave).To(Equal(uint8(4)))
+}
+
 func TestXMLConvert(t *testing.T) {
 	g := NewWithT(t)
 
-	doc, err := balafon.ToXML(input)
-	g.Expect(err).NotTo(HaveOccurred())
+	b, err := balafon.ToXML([]byte(`
+:channel 9
+:assign k 36
+:assign s 38
 
-	b, err := xml.MarshalIndent(doc, "", "    ")
+:channel 0
+:assign c 60
+
+:timesig 4 4
+
+:bar one
+	:channel 9
+	[- s - s]
+	[k k k k]
+
+	:channel 0
+	c2   c2
+:end
+
+:bar two
+	:channel 9
+	-8 s.      s8. s16
+	[k    k    k  k]
+
+	:channel 0
+	c2.    c
+:end
+
+:play one
+:play two
+`))
+
 	g.Expect(err).NotTo(HaveOccurred())
 
 	fmt.Println(string(b))
