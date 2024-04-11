5bf90580d18470d51febe18ef5c772f973580368
Magnus Kokk <magnus@kokk.eu>
2024-04-12 00:25:59 +0300

    Move testdata generate command to testdata

--- gen.go
+++
@@ -1,3 +0,0 @@
-//go:generate go run cmd/balafon/main.go smf -o testdata/bonham.mid examples/bonham.bal
-
-package balafon
---
+++ testdata/gen.go
@@ -0,0 +1,3 @@
+//go:generate go run ../cmd/balafon/main.go smf -o bonham.mid ../examples/bonham.bal
+
+package testdata
